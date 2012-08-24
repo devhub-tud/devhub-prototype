f62b54ed000f733f845071f69f3c1b37ecc6ecb5
Alex Nederlof <alex@nederlof.com>
2012-08-24 16:19:37 +0200

    Added code samples for the other Java 7 featuers

--- dev-env-admin-web/src/test/java/nl/tudelft/ew/dea/Java7Compatibility.java
+++ dev-env-admin-web/src/test/java/nl/tudelft/ew/dea/Java7Compatibility.java
@@ -1,12 +1,30 @@
 package nl.tudelft.ew.dea;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.junit.Assert;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Test if you are working with Java 7.
  * 
  */
 public class Java7Compatibility {
+
+	private static final Logger LOG = LoggerFactory.getLogger(Java7Compatibility.class);
 
 	@Test
 	public void testStringSwitch() {
@@ -17,5 +35,47 @@ public class Java7Compatibility {
 			default:
 				Assert.fail("Aap should have been asserted");
 		}
+	}
+
+	@Test
+	public void variablesWithUnderscores() {
+		long someNumber = 111_001L;
+		long withoutUnderscores = 111001L;
+		assertEquals(someNumber, withoutUnderscores);
+	}
+
+	@Test
+	public void easyCollectionMaking() {
+		List<Map<Date, String>> listOfMaps = new ArrayList<>();
+		HashMap<Date, String> aMap = new HashMap<>();
+		aMap.put(new Date(), "Hello");
+		listOfMaps.add(aMap);
+	}
+
+	@Test
+	public void testMultiCatch() {
+		try {
+			Class string = Class.forName("java.lang.String");
+			string.getMethod("length").invoke("test");
+		} catch (ClassNotFoundException |
+				IllegalAccessException |
+				IllegalArgumentException |
+				InvocationTargetException |
+				NoSuchMethodException |
+				SecurityException e) {
+			fail("Should not have been initialized");
+		}
+	}
+
+	@Test
+	public void tryWithResource() throws Exception {
+		String file1 = "target/TryWithResourceFile.out";
+		try (OutputStream out = new FileOutputStream(file1)) {
+			out.write("Some silly file content ...".getBytes());
+			":-p".charAt(3);
+		} catch (StringIndexOutOfBoundsException | IOException e) {
+			LOG.info("Exception on operating file " + file1 + ": " + e.getMessage());
+		}
+		// Out automagically closes now
 	}
 }
