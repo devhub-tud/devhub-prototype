106a21dc99527fc2ac19b73227a8c5f5098054f4
Alex Nederlof <alex@nederlof.com>
2012-09-11 18:15:19 +0200

    Added tests for resetting password

---
+++ web/src/test/java/nl/tudelft/ewi/dea/jaxrs/account/AccountResourceTest.java
@@ -0,0 +1,71 @@
+package nl.tudelft.ewi.dea.jaxrs.account;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import javax.inject.Provider;
+
+import nl.tudelft.ewi.dea.dao.PasswordResetTokenDao;
+import nl.tudelft.ewi.dea.dao.UserDao;
+import nl.tudelft.ewi.dea.jaxrs.utils.Renderer;
+import nl.tudelft.ewi.dea.model.User;
+import nl.tudelft.ewi.dea.security.SecurityProvider;
+import nl.tudelft.ewi.dea.security.UserFactory;
+
+import org.apache.shiro.authz.AuthorizationException;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class AccountResourceTest {
+
+	private final long userId = 1;
+	@Mock private Provider<Renderer> renderers;
+	@Mock private PasswordResetTokenDao passwordResetTokenDao;
+	@Mock private UserDao userDao;
+	@Mock private UserFactory userFactory;
+	@Mock private SecurityProvider subjectProvider;
+	@Mock private User user;
+	@InjectMocks private AccountResource resource;
+
+	@Before
+	public void before() {
+		when(subjectProvider.getUser()).thenReturn(user);
+		when(user.getId()).thenReturn(userId);
+	}
+
+	@Test
+	public void whenUserIsAllowedUpdateHisPassword() {
+		String password = "newPassword";
+		NewPasswordRequest request = mock(NewPasswordRequest.class);
+
+		when(request.getPassword()).thenReturn(password);
+		resource.resetPassword(userId, request);
+
+		verify(userFactory).resetUserPassword(user, password);
+	}
+
+	@Test(expected = AuthorizationException.class)
+	public void prohibitNormalUserFromChangingOtherPassword() {
+		long otherUserId = 2l;
+		resource.resetPassword(otherUserId, null);
+	}
+
+	@Test
+	public void allowAdminToResetOtherPassword() {
+		String password = "newPassword";
+		NewPasswordRequest request = mock(NewPasswordRequest.class);
+		when(user.isAdmin()).thenReturn(true);
+		long otherUserId = 2l;
+		when(request.getPassword()).thenReturn(password);
+		resource.resetPassword(otherUserId, request);
+
+		verify(userFactory).resetUserPassword(user, password);
+	}
+
+}
