437be19e8c35fc1d7de514ad01edcc27d3a32f5e
David Hartveld <david@hartveld.com>
2012-09-14 00:33:53 +0200

    Added javax.persistence.NoResultExeption JAX-RS response mapper. Added logging in UnauthorizedExceptionMapper.

--- web/src/main/java/nl/tudelft/ewi/dea/jaxrs/exceptions/mappers/UnauthorizedExceptionMapper.java
+++ web/src/main/java/nl/tudelft/ewi/dea/jaxrs/exceptions/mappers/UnauthorizedExceptionMapper.java
@@ -6,13 +6,23 @@ import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
 import org.apache.shiro.authz.UnauthorizedException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Provider
 public class UnauthorizedExceptionMapper implements ExceptionMapper<UnauthorizedException> {
 
+	private static final Logger LOG = LoggerFactory.getLogger(UnauthorizedExceptionMapper.class);
+
 	@Override
-	public Response toResponse(UnauthorizedException ex) {
-		return Response.status(Status.UNAUTHORIZED).build();
+	public Response toResponse(final UnauthorizedException exception) {
+
+		LOG.debug("Mapping exception to HTTP 401 Unauthorized: {}", exception.getMessage(), exception);
+
+		return Response.status(Status.UNAUTHORIZED)
+				.entity("Current user is not authorized to access this page")
+				.build();
+
 	}
 
 }
