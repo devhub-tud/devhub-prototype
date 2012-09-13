146ef22d802f430288169abb36f3e988a27cadd1
David Hartveld <david@hartveld.com>
2012-09-13 16:07:39 +0200

    Added stubs to ProjectResource for enroll-in-course story.

--- web/src/main/java/nl/tudelft/ewi/dea/jaxrs/projects/ProjectResource.java
+++ web/src/main/java/nl/tudelft/ewi/dea/jaxrs/projects/ProjectResource.java
@@ -1,9 +1,11 @@
 package nl.tudelft.ewi.dea.jaxrs.projects;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -24,6 +26,59 @@ public class ProjectResource {
 	public Response serveProjectPage(@PathParam("id") final long id) {
 
 		LOG.trace("Serving page for project: {} - not yet implemented", id);
+
+		// TODO Implement serving project overview page.
+
+		return Response.serverError().entity("Not yet implemented. Go to the <a href=\"/dashboard\">dashboard</a>.").build();
+
+	}
+
+	@GET
+	@Path("{projectId}/invite/{userId}")
+	public Response inviteUser(@PathParam("projectId") final long projectId, @PathParam("userId") final long userId) {
+
+		LOG.trace("Inviting user {} for project {} - not yet implemented", userId, projectId);
+
+		// TODO Implement inviting other users.
+
+		return Response.serverError().entity("Not yet implemented. Go to the <a href=\"/dashboard\">dashboard</a>.").build();
+
+	}
+
+	@GET
+	@Path("{id}/invitation")
+	@Produces(MediaType.TEXT_HTML)
+	public Response serveInvitationPage(@PathParam("id") final long id) {
+
+		LOG.trace("Serving invitation page for project: {} - Not yet implemented", id);
+
+		// TODO Implement serving of page. Could also be implemented client-side
+		// as a pop-up.
+
+		return Response.serverError().entity("Not yet implemented. Go to the <a href=\"/dashboard\">dashboard</a>.").build();
+
+	}
+
+	@POST
+	@Path("{id}/invitation")
+	public Response answerInvitation(@PathParam("id") final long id, @QueryParam("accept") final boolean accept) {
+
+		LOG.trace("Answering invitation: {} - accept? {} - Not yet implemented", id, accept);
+
+		// TODO: Accept or reject invitation.
+
+		return Response.serverError().entity("Not yet implemented. Go to the <a href=\"/dashboard\">dashboard</a>.").build();
+
+	}
+
+	@GET
+	@Path("{id}/provision")
+	public Response provisionProject(final long id) {
+
+		LOG.trace("Provisioning project: {} - Not yet implemented", id);
+
+		// TODO: Check that all invitations are answered (accepted/rejected).
+		// TODO: Provision project.
 
 		return Response.serverError().entity("Not yet implemented. Go to the <a href=\"/dashboard\">dashboard</a>.").build();
 
