551d36265676b6a3aba483686c30abc2108681d3
Michael de Jong <michaelj@minicom.nl>
2012-11-22 14:32:17 +0100

    Making /version work again.

--- web/src/main/java/nl/tudelft/ewi/dea/jaxrs/html/RootPage.java
+++ web/src/main/java/nl/tudelft/ewi/dea/jaxrs/html/RootPage.java
@@ -2,9 +2,14 @@ package nl.tudelft.ewi.dea.jaxrs.html;
 
 import java.net.URI;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
+import nl.tudelft.ewi.dea.BuildInfo;
 
 import com.google.inject.servlet.RequestScoped;
 
@@ -12,9 +17,23 @@ import com.google.inject.servlet.RequestScoped;
 @Path("/")
 public class RootPage {
 
+	private final BuildInfo version;
+
+	@Inject
+	RootPage(BuildInfo version) {
+		this.version = version;
+	}
+
 	@GET
 	public Response redirectToDashboard() {
 		return Response.seeOther(URI.create("/dashboard")).build();
+	}
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("version")
+	public BuildInfo version() {
+		return version;
 	}
 
 }
