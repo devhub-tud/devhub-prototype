fd83e1bb11f34aa68f7833ca73adf01f821cc5d4
Alex Nederlof <alex@nederlof.com>
2013-02-01 16:48:55 +0100

    Updated to new versions. Enabled a new profile named DEV

--- devhub-server/redeploy-test.sh
+++ devhub-server/redeploy-test.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 USER="devhub"
-HOST="devhub.hartveld.com"
+HOST="dev.devhub.nl"
 
 set -e
 mvn clean install
 cd web
-mvn clean package -DskipTests=true -P production
+mvn clean package -DskipTests=true -P dev
 cd ..
 scp web/target/devhub.war $USER@$HOST:~
 ssh $USER@$HOST sh /home/$USER/startDevhub.sh restart
